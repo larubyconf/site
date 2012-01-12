1523637eb31b15793bd269d0ad186fec17719f8b
Coby Randquist <randquistcp@gmail.com>
2012-01-12 09:03:30 -0800

    centering page logo if used

--- app/helpers/sponsors_helper.rb
+++ app/helpers/sponsors_helper.rb
@@ -3,7 +3,7 @@ module SponsorsHelper
     if sponsor.logo_display
       content_tag(:p, link_to(image_tag("#{sponsor.logo.url}"), sponsor.url, :target => "_blank"))
     else
-      content_tag(:p, link_to(image_tag("#{sponsor.page_logo.url}"), sponsor.url, :target => "_blank")) unless sponsor.page_logo_file_name.nil?
+      content_tag(:p, link_to(image_tag("#{sponsor.page_logo.url}"), sponsor.url, :target => "_blank"), :class => 'center') unless sponsor.page_logo_file_name.nil?
     end
   end
 end
